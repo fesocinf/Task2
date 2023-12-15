841c43a3a1eb9c2a9d61da0ac25118b56e2408c9
d.ryabkov <test>
2023-12-15 21:04:23 +0300

    add cat bean

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,17 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld beanCopy =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.equals(beanCopy));
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+        Cat catBeanAnother =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.equals(catBeanAnother));
+
         System.out.println(bean.getMessage());
+        System.out.println(catBean.getName());
     }
 }
